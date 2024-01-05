31a4f450f11d79c66e5ebaa38b343838eb3259b0
Vinson Xavier <vinson.xavier@viewzenlabs.com>
2024-01-05 18:12:37 +0530

    initialization

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,12 @@
 import express from "express";
 import morgan from "morgan";
 
+import { router } from "./routers/playerRouter";
+
 const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 
-console.log(process.env);
-
-app.get("/", (req, res) => {
-  res.send("Hello from typescript");
-});
-
-const port = process.env.PORT || 8000;
-app.listen(port, () => console.log(`Server is listening on port ${port}`));
+router.use("/api/players/");
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -7,3 +7,6 @@ import app from "./app";
 mongoose
   .connect(process.env.DB)
   .then(() => console.log("DB connected successfully"));
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
